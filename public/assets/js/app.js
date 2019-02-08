bca60052218c2980cc3b46d72a7284f38610ba16
e-g-jackson <42052471+e-g-jackson@users.noreply.github.com>
2019-02-08 14:04:01 -0600

    add feedURL

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -12,6 +12,7 @@ $(search).keyup(function(){
 //https://itunes.apple.com/search?parameterkeyvalue
 //key1=value1&amp;key2=value2&amp;key3=value3
 // var url2 = 'https://itunes.apple.com/search?term=jack+johnson&entity=musicVideo&country=us';
+
 $(sBtn).on('click', function () {
     input = input.replace(/\s+/g, '+');
     url = 'https://itunes.apple.com/search?term=' + input + '&media=podcast&attribute=descriptionTerm&country=us&entity=podcast&limit=10';
@@ -38,10 +39,12 @@ function renderer(res, num){
     var genre = res.genres;
     var genreP = '<p>Genre: <strong>' + genre.join(', ') + '</strong></p>';
     var link = res.trackViewUrl;
+    var feedLink = '<a href = ' + res.feedURL + '>Feed URL</a>';
     // for ( var j = 0; j < genre.length; j++){
         
     // }
-    var template = $('<p>Podcast Title: <strong>' + title + '</strong></p> ' + genreP + ' <p>Author: <strong>' + author + '</strong></p> <p>Link: <a href = \'' + link + '\'<em>' + link + '</em></a></p> <hr>');
+    var template = $('<p>Podcast Title: <strong>' + title + '</strong></p> ' + genreP + ' <p>Author: <strong>' + author + '</strong></p> ');
+    template += '<p>' + feedLink + '</p><p>Link: <a href = \'' + link + '\'<em>' + link + '</em></a></p> <hr>'
     $(pic).appendTo(div);
     $(template).appendTo(div);
     $(div).appendTo(rsltDiv);
